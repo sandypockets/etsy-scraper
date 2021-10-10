fdfd7d9824fcd52b435663e988333835c8764464
sandypockets <zac@znacreative.com>
2021-10-09 23:43:37 -0400

    Add sales, description, processing time

--- pageScraper.js
+++ pageScraper.js
@@ -32,8 +32,14 @@ const scraperObject = {
       let newPage = await browser.newPage()
       await newPage.goto(link)
       console.log(`Navigating to ${link}...`)
+      await page.waitForSelector("#content")
       dataObj["title"] = await newPage.$eval("#listing-page-cart > div.wt-mb-xs-2 > h1", text => text.textContent)
       dataObj["price"] = await newPage.$eval("#listing-page-cart > div > div > div > div > div > p", text => text.textContent)
+      // Not all products are on sale
+      // dataObj["originalPrice"] = await newPage.$eval("#listing-page-cart > div > div > div > div > div > p.wt-text-strikethrough", text => text.textContent)
+      dataObj["numberOfSales"] = await newPage.$eval("#listing-page-cart > div > div > div > div > span.wt-text-caption", text => text.textContent)
+      dataObj["description"] = await newPage.$eval("#wt-content-toggle-product-details-read-more > p", text => text.textContent)
+      dataObj["processingTime"] = await newPage.$eval("#shipping-variant-div > div > div.wt-grid > div > p", text => text.textContent)
       dataObj["url"] = link
       resolve(dataObj)
       reject(dataObj)
@@ -48,12 +54,14 @@ const scraperObject = {
     for (let data of this.scrapedData) {
       let url = data.url
       let title = data.title
+
       if (title) {
         title.toString().trim()
         title = title.slice(28) // Removes 'read the full title'
         title = title.split("\n")
         title = title[0]
       }
+
       let price = data.price
       if (price) {
         price.toString().trim()
@@ -63,7 +71,24 @@ const scraperObject = {
           price = Number(price)
         }
       }
-      this.formattedData.push({ price, title, url })
+
+      // let originalPrice = data.originalPrice
+      // originalPrice.toString().trim()
+      // console.log("ORIGINAL PRICE: ", originalPrice)
+
+      // Represents how many sales the seller has in total, not the number of products
+      let numberOfSales = data.numberOfSales
+      numberOfSales.toString().trim()
+      console.log("NUMBER OF SALES: ", numberOfSales)
+
+      let description = data.description
+      description = description.toString().trim()
+      console.log("DESCRIPTION: ", description)
+
+      let processingTime = data.processingTime
+      processingTime = processingTime.toString().trim()
+
+      this.formattedData.push({ title, price, url, description, numberOfSales, processingTime })
 
     }
     console.log("The CSV is being prepared...")
